0f196de5fb96a992f176a608094cd3770cc57588
ivanprytula <johnnyforester@gmail.com>
2020-01-03 13:36:02 +0200

    Change project name from lomac > lpmac Install python-decouple, DRF, cors-headers; Add .env./env.example with few decoupled settings for secret _ey, db and allowed_hosts

--- lpmac/wsgi.py
+++ lpmac/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for lomac project.
+WSGI config for lpmac project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lomac.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lpmac.settings')
 
 application = get_wsgi_application()
